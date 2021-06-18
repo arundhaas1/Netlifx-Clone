8102a305af0ab86cb176f0c660f9fc83f88935fe
arundhaas1 <arunram.9215@gmail.com>
2021-06-18 22:42:24 +0530

    data

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -11,20 +11,23 @@ function Login() {
           src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
         />
         <button className="login__button">Sign In</button>
-        <div className="login__gradient"/>
+        <div className="login__gradient" />
       </div>
       <div className="login__body">
-            <>
-            <h1>Unlimited films,Tv programmes and more.</h1>
-            <h2>Watch anywhere. Cancel at any time</h2>
-            <h3>Ready to watch ? Enter Your Email to craete or restart your Membership</h3>
-            <div className="login__input">
-                <form>
-                    <input type="email" placeholder="Email Address"/>
-                    <button className="getstarted">GET STARTED</button>
-                </form>
-            </div>
-            </>
+        <>
+          <h1>Unlimited films,Tv programmes and more.</h1>
+          <h2>Watch anywhere. Cancel at any time</h2>
+          <h3>
+            Ready to watch ? Enter Your Email to craete or restart your
+            Membership
+          </h3>
+          <div className="login__input">
+            <form>
+              <input type="email" placeholder="Email Address" />
+              <button className="getstarted">GET STARTED</button>
+            </form>
+          </div>
+        </>
       </div>
     </div>
   );
